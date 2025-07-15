86356f74456f94005f62dbd9766e85840d24c72b
Dhanush098343 <bolisettydhanush@GMAIL.COM>
2025-07-15 18:27:47 -0400

    Update generate_token.py

--- generate_token.py
+++ generate_token.py
@@ -3,11 +3,11 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 SCOPES = ['https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/spreadsheets']
 
 def main():
-    flow = InstalledAppFlow.from_client_secrets_file("client_secrets.json", SCOPES)
+    flow = InstalledAppFlow.from_client_secrets_file("/etc/secrets/client_secrets.json", SCOPES)
     creds = flow.run_local_server(port=0)
     
     # Save token as JSON
-    with open("token.json", "w") as token:
+    with open("/etc/secrets/token.json", "w") as token:
         token.write(creds.to_json())
 
 if __name__ == "__main__":
